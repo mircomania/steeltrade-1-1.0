fb16661e610d17b238455787bd95ceb66fe816b4
mircomania <mirco.rodriguez@hotmail.com>
2025-04-29 16:47:58 -0400

    2 commit steeltrade - maqueta section 1 1.0

--- src/components/common/BurgerMenu.jsx
+++ src/components/common/BurgerMenu.jsx
@@ -1,32 +1,88 @@
+import { navLinks } from '../utils/navBarMenu';
+
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-import { navLinks } from '../utils/navBarMenu';
-
 export const BurgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleLinkClick = (e, link) => {
+        e.preventDefault();
+
+        if (location.pathname === '/') {
+            // Si ya estás en la página principal, realiza el scroll a la sección
+            const targetElement = document.querySelector(link.to);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop, // Ajuste para compensar el navbar
+                    behavior: 'smooth',
+                });
+            }
+        } else {
+            // Si estás en otra página, navega a la landing page con el hash
+            navigate(`/${link.to}`);
+        }
+
+        toggleMenu(); // Cerrar el menú hamburguesa
+    };
+
+    const handleInicioClick = (e) => {
+        e.preventDefault();
+
+        if (location.pathname === '/') {
+            // Ya estás en la página principal, scroll al top
+            const topElement = document.querySelector('#inicio') || document.body;
+            topElement.scrollIntoView({ behavior: 'smooth' });
+        } else {
+            // Navegar a la página principal
+            navigate('/#inicio');
+        }
+
+        toggleMenu(); // Cerrar el menú hamburguesa
+    };
+
     return (
         <div className="burger-menu">
-            <FontAwesomeIcon icon={faBars} className="burger-menu-icon" aria-label="Abrir o cerrar menú" onClick={toggleMenu} />
+            {/* Icono de barras para abrir el menú */}
+            <FontAwesomeIcon icon={faBars} className="burger-menu-icon" onClick={toggleMenu} />
 
             {/* Contenedor del menú móvil */}
             <div className={`mobile-nav-menu ${isOpen ? 'open' : ''}`}>
                 {/* Opciones del menú */}
-                <ul className="burger-menu-nav light-text">
-                    {navLinks.map((link) => (
+                <ul className="burger-menu-nav bold-text">
+                    {navLinks.map((link, index) => (
                         <div className="link-container" key={link.id}>
-                            <li>
-                                <NavLink to={link.to} onClick={toggleMenu} title={link.title}>
-                                    {link.label}
-                                </NavLink>
+                            <li className={index === navLinks.length - 1 ? 'last-item' : ''}>
+                                {(() => {
+                                    if (link.id === 'inicio') {
+                                        return (
+                                            <a href="/" onClick={handleInicioClick} title={link.title}>
+                                                {link.label}
+                                            </a>
+                                        );
+                                    } else if (link.to.startsWith('#')) {
+                                        return (
+                                            <a href={link.to} onClick={(e) => handleLinkClick(e, link)} title={link.title}>
+                                                {link.label}
+                                            </a>
+                                        );
+                                    } else {
+                                        return (
+                                            <NavLink to={link.to} onClick={toggleMenu} title={link.title}>
+                                                {link.label}
+                                            </NavLink>
+                                        );
+                                    }
+                                })()}
                             </li>
                         </div>
                     ))}
--- src/components/imports/SectionLp1.jsx
+++ src/components/imports/SectionLp1.jsx
@@ -3,7 +3,18 @@ import styles from '../../styles/modules/sectionLp1.module.css';
 export const SectionLp1 = () => {
     return (
         <section className={styles.sectionContainer}>
-            <h1>SECTION 1</h1>
+            <div className={styles.sectionTitulo}>
+                <h1 className="bold-text">LIDER GLOBAL</h1>
+
+                <p className="light-text">
+                    Contamos con más de 30 años de experiencia, en G Steel Trade te garantizamos soluciones logísticas eficientes y el suministro
+                    confiable de acero de alta calidad para todo el mercado internacional. Nuestro compromiso con la rentabilidad, la flexibilidad y
+                    el servicio post venta sin duda nos posiciona como un aliado estratégico para tu negocio.
+                </p>
+            </div>
+
+            {/* Fondos animados */}
+            <div className={`${styles.bg} ${styles.bg1}`}></div>
         </section>
     );
 };
